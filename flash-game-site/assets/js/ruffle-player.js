278a4ad2ad426438c6657611a81a34fcfcdab504
egram jason <egramjason@6-A-YamamotoMacBook-Pro.local>
2026-09-23 16:08:39 +0900

    Add flash game site

---
+++ flash-game-site/assets/js/ruffle-player.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const container =
+        document.getElementById("ruffle-container");
+
+    if (!container) {
+        return;
+    }
+
+    const ruffle =
+        window.RufflePlayer.newest();
+
+    const player =
+        ruffle.createPlayer();
+
+    player.style.width = "800px";
+    player.style.height = "600px";
+
+    container.appendChild(player);
+
+    player.ruffle().load("cr_miyoco_dw_8.swf");
+});
